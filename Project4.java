c5697c66b5259aa4ae3b8c97d2fc88ca9ecfc9d6
Shivam Kumar Yadav <112298894+Shivam7061@users.noreply.github.com>
2022-11-15 10:22:29 +0530

    Add files via upload

---
+++ Project4.java
@@ -0,0 +1,27 @@
+package Projects;
+
+import java.util.Scanner;
+
+public class Project4 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter your Physics marks : ");
+		int physics = scan.nextInt();
+		System.out.println("Enter your English marks : ");
+		int English = scan.nextInt();
+		System.out.println("Enter your Chemistry marks : ");
+		int chemistry = scan.nextInt();
+		System.out.println("Enter your Mathematics marks : ");
+		int mathematics = scan.nextInt();
+		System.out.println("Enter your Computer Science marks : ");
+		int computer = scan.nextInt();
+
+		float percentage = ((physics + English + chemistry + mathematics + computer) / 500.0f) * 100;
+
+		System.out.println("percentage : ");
+		System.out.println(percentage);
+
+	}
+}
